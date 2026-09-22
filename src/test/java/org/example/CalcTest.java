6cd0fca1d728d3e4e8f5f980fe47e3e26743c60b
Yang-ximeng <728540954@qq.com>
2026-09-22 23:10:54 +0800

    Add failing subtraction test

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -10,5 +10,10 @@ public class CalcTest {
     void testAddition() {
         assertEquals(4, c.add(2,2));
     }
-
+    @Test
+    public void testSubtraction() {
+        Calc c = new Calc();
+        // 故意写错！假设 subtract(4, 2) 应该等于 2，我们偏要说它等于 3，让测试失败
+        assertEquals(2, c.subtract(4, 2));
+    }
 }
